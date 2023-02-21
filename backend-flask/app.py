bf377c630637653172519a94f2a2f1051449d3c2
Gloria Macia <gloriamaciamunoz@gmail.com>
2023-02-21 23:49:51 +0000

    add notifications backend and frotened

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -12,6 +12,7 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+from services.notifications_activities import *
 
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
@@ -63,6 +64,11 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
+  return data, 200
+
+@app.route("/api/activities/notifications", methods=['GET'])
+def data_notifications():
+  data = NotificationsActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
