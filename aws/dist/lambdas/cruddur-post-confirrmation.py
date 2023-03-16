296cb8102260bacd59fe1faa856d0849894ad1be
Gloria Macia <gloriamaciamunoz@gmail.com>
2023-03-16 20:57:02 +0000

    fix post confirmation tupple error in lambda

--- aws/dist/lambdas/cruddur-post-confirrmation.py
+++ aws/dist/lambdas/cruddur-post-confirrmation.py
@@ -13,33 +13,24 @@ def lambda_handler(event, context):
     user_cognito_id    = user['sub']
     try:
       print('entered-try')
-      sql = f"""
-         INSERT INTO public.users (
-          display_name, 
-          email,
-          handle, 
-          cognito_user_id
-          ) 
-          VALUES('{user_display_name}', '{user_email}', '{user_handle}', '{user_cognito_id}')
-      """
-      print('SQL Statement ----')
-      print(sql)
       conn = psycopg2.connect(os.getenv('PROD_CONNECTION_URL'))
       cur = conn.cursor()
+      cur.execute("INSERT INTO users (display_name, email, handle, cognito_user_id) VALUES(%s, %s, %s, %s)", (user['name'], user['email'], user['preferred_username'], user['sub']))
       params = [
         user_display_name,
         user_email,
         user_handle,
         user_cognito_id
       ]
-      cur.execute(sql,*params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
-      print(error)
+        print(error)
+        
     finally:
-      if conn is not None:
-          cur.close()
-          conn.close()
-          print('Database connection closed.')
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+
     return event
