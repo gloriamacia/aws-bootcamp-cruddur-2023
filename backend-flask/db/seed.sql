45f94622c0c62e44fac43fe709315591a42afc74
Gloria Macia <gloriamaciamunoz@gmail.com>
2023-03-23 20:58:21 +0000

    fix json error

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('Gloria Macia', 'gloria.macia@gmail.com','gloria','MOCK');
+  ('sergi','gloria.macia@roche.com' , 'sergimichael' ,'MOCK'),
+  ('Gloria Macia', 'gloriamaciamunoz@gmail.com','gloriamacia','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'gloriamacia' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
