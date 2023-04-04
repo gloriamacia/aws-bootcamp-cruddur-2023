59f04c270367583374faca89c3fa5e93620625cb
Gloria Macia <gloriamaciamunoz@gmail.com>
2023-04-04 10:17:56 +0000

    fix coginto

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -1,17 +1,22 @@
-import './MessageGroupsPage.css';
+import './MessageGroupPage.css';
 import React from "react";
+import { useParams } from 'react-router-dom';
 
+import {checkAuth, getAccessToken} from '../lib/CheckAuth';
 import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
-import {checkAuth, getAccessToken} from '../lib/CheckAuth';
+import MessagesFeed from '../components/MessageFeed';
+import MessagesForm from '../components/MessageForm';
 
-export default function MessageGroupsPage() {
+export default function MessageGroupPage() {
   const [messageGroups, setMessageGroups] = React.useState([]);
+  const [messages, setMessages] = React.useState([]);
   const [popped, setPopped] = React.useState([]);
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
+  const params = useParams();
 
-  const loadData = async () => {
+  const loadMessageGroupsData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
       await getAccessToken()
@@ -33,12 +38,35 @@ export default function MessageGroupsPage() {
     }
   };  
 
+  const loadMessageGroupData = async () => {
+    try {
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
+      const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        },
+        method: "GET"
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessages(resJson)
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };  
+
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
-    loadData();
+    loadMessageGroupsData();
+    loadMessageGroupData();
     checkAuth(setUser);
   }, [])
   return (
@@ -47,7 +75,9 @@ export default function MessageGroupsPage() {
       <section className='message_groups'>
         <MessageGroupFeed message_groups={messageGroups} />
       </section>
-      <div className='content'>
+      <div className='content messages'>
+        <MessagesFeed messages={messages} />
+        <MessagesForm setMessages={setMessages} />
       </div>
     </article>
   );
