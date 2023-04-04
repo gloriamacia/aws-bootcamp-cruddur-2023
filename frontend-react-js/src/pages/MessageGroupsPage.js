5322367fbd19ef3ad6cb8ffdbe2fe6fc66f51ad4
Gloria Macia <gloriamaciamunoz@gmail.com>
2023-04-04 10:11:03 +0000

    add cognito part complete MessageGroupPage.js has extra code from future

--- frontend-react-js/src/pages/MessageGroupsPage.js
+++ frontend-react-js/src/pages/MessageGroupsPage.js
@@ -3,7 +3,7 @@ import React from "react";
 
 import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
-import checkAuth from '../lib/CheckAuth';
+import {checkAuth} from '../lib/CheckAuth';
 
 export default function MessageGroupsPage() {
   const [messageGroups, setMessageGroups] = React.useState([]);
