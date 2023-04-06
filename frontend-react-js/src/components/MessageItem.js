5bf88a5e03c4153a3888bc5e3f2d272ed277ba87
Gloria Macia <gloriamaciamunoz@gmail.com>
2023-04-06 13:58:11 +0000

    minute 15:50

--- frontend-react-js/src/components/MessageItem.js
+++ frontend-react-js/src/components/MessageItem.js
@@ -4,25 +4,9 @@ import { format_datetime, message_time_ago } from '../lib/DateTimeFormats';
 
 export default function MessageItem(props) {
 
-  const format_time_created_at = (value) => {
-    // format: 2050-11-20 18:32:47 +0000
-    const created = DateTime.fromISO(value)
-    const now     = DateTime.now()
-    const diff_mins = now.diff(created, 'minutes').toObject().minutes;
-    const diff_hours = now.diff(created, 'hours').toObject().hours;
-
-    if (diff_hours > 24.0){
-      return created.toFormat("LLL L");
-    } else if (diff_hours < 24.0 && diff_hours > 1.0) {
-      return `${Math.floor(diff_hours)}h`;
-    } else if (diff_hours < 1.0) {
-      return `${Math.round(diff_mins)}m`;
-    }
-  };
-
   return (
-    <Link className='message_item' to={`/messages/@`+props.message.handle}>
-      <div className='message_avatar'></div>
+    <div className='message_item'>
+      <Link className='message_avatar' to={`/messages/@`+props.message.handle}></Link>
       <div className='message_content'>
         <div classsName='message_meta'>
           <div className='message_identity'>
@@ -35,9 +19,6 @@ export default function MessageItem(props) {
         <span className='ago'>{message_time_ago(props.message.created_at)}</span> 
         </div>{/* created_at */}
       </div>{/* message_content */}
-    </Link>
+    </div>
   );
 }
-
-
-
