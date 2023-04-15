a386d053b15e4a3f3e81ae15b831c33adf85dc9f
Gloria Macia <gloriamaciamunoz@gmail.com>
2023-04-15 14:48:33 +0000

    run migrations when we do setup, generating out a presigned url

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -11,10 +11,14 @@ def handler(event:, context:)
   url = obj.presigned_url(:put, expires_in: 60 * 5)
   url # this is the data that will be returned
   body = {url: url}.to_json
-  { statusCode: 200, body: body }
+  { 
+    headers: {
+      "Access-Control-Allow-Headers": "*, Authorization",
+      "Access-Control-Allow-Origin": "https://3000-gloriamacia-awsbootcamp-ubjx83ud7rt.ws-eu94.gitpod.io/",
+      "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+    },
+    statusCode: 200, 
+    body: body 
+    
+  }
 end
-
-puts handler(
-  event: {},
-  context: {}
-)
